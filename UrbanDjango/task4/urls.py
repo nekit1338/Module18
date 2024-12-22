14a12684e13f0ee59aa8168a6f1ffa88b6a1f139
nekit1338 <47562425+nekit1338@users.noreply.github.com>
2024-12-23 03:36:45 +0500

    Update urls.py
    В task4.views для главной страницы  сделал префикс "main", чтобы избежать конфликтов у обоих приложений task4 и task5 при обращении к одному адресу "http://127.0.0.1:8000"

--- UrbanDjango/task4/urls.py
+++ UrbanDjango/task4/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='main'),
+    path('main', views.index, name='main'),
     path('shop', views.index_2, name='shop'),
     path('cart', views.index_3, name='cart'),
     path('add_to_cart', views.add_to_cart, name='add_to_cart'),
